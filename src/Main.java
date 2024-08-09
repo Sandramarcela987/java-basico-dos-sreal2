aadc52fb3a47f131b25715c44815d76d0ad9664c
sandramarcela987 <sandramarcela987@gmail.com>
2024-08-08 21:07:31 -0500

    Java-basico2-ejercicio-dos-sreal

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int A = 0;
+        int B = 1;
+
+        while (B <= 10) {
+            A += B;
+            B++;
+            System.out.println(" LA SUMATORIA ES :" + A);
+        }
+
+    }
+}
